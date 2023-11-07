f49d4b9dedc47281480dcb5169b0509ed782ed51
Darius <darius.jurjiu@yahoo.com>
2023-11-07 18:35:00 +0200

    Added admin dashboard

--- app/backend/app/models/__init__.py
+++ app/backend/app/models/__init__.py
@@ -1,3 +1,6 @@
 from .user import *
 from .diagnostic import *
 from .clinic import *
+
+
+admin_fields = {("management-" + cls.__name__): cls for cls in [Patient, Doctor]}
--- app/backend/run.py
+++ app/backend/run.py
@@ -3,6 +3,9 @@ from app.controller import blueprints
 from flask_migrate import Migrate
 from database import db
 from flask_cors import CORS
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+import app.models as models
 import os
 
 
@@ -15,11 +18,14 @@ def create_app():
     for bp in blueprints:
         app.register_blueprint(bp)
     CORS(app)
+    admin = Admin(app, name="management", template_mode="bootstrap3", url="/dashboard")
+    for endpoint_name, table in models.admin_fields.items():
+        admin.add_view(
+            ModelView(table, db.session, name=endpoint_name, url=endpoint_name)
+        )
 
     return app
 
-
-import app.models
 
 app = create_app()
 migrate = Migrate(
