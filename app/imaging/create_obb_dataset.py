2da058143f3451e61bb6c4a95db3a989ab4c851a
Darius <darius.jurjiu@yahoo.com>
2024-02-29 18:26:00 +0200

    Added coordinate nromalization.

--- app/imaging/create_obb_dataset.py
+++ app/imaging/create_obb_dataset.py
@@ -10,6 +10,22 @@ IMAGE_WIDTH = 1720
 IMAGE_HEIGHT = 1280
 
 
+def extract_normalized_coords(box):
+    x1, y1 = box[0]
+    x2, y2 = box[1]
+    x3, y3 = box[2]
+    x4, y4 = box[3]
+    x1 = x1 / IMAGE_WIDTH
+    x2 = x2 / IMAGE_WIDTH
+    x3 = x3 / IMAGE_WIDTH
+    x4 = x4 / IMAGE_WIDTH
+    y1 = y1 / IMAGE_HEIGHT
+    y2 = y2 / IMAGE_HEIGHT
+    y3 = y3 / IMAGE_HEIGHT
+    y4 = y4 / IMAGE_HEIGHT
+    return [x1, y1, x2, y2, x3, y3, x4, y4]
+
+
 def transform_dataset():
     all_files = os.listdir(ANNOTATIONS_LOCATION)
 
@@ -27,7 +43,9 @@ def transform_dataset():
 
             for d in data:
                 # The bounding boxes already have the right absolute values, because they were resized at size(1720, 1280) at extraction
-                f.write(f"0 {' '.join([str(point) for point in d])}\n")
+                f.write(
+                    f"0 {' '.join([str(coord) for coord in extract_normalized_coords(d)])}\n"
+                )
 
 
 if __name__ == "__main__":
